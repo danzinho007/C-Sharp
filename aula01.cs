79aab733595ab08d107648a27329e75aa64fa077
danzinho007 <sedutordanzinho@hotmail.com>
2023-03-01 02:04:25 -0300

    adicionei

--- aula01.cs
+++ aula01.cs
@@ -1,6 +1,27 @@
 using System;
+
+namespace HelloWord
+{
 class Principal{
     static void Main(){
-        Console.Write("Olá Mundo!");
+        Console.WriteLine("Ola Mundo!");
+        Console.WriteLine("Me livrei da maldicao");
+        Console.Write("Somando 3 com 3 da : ");
+        Console.WriteLine(3 + 3);
+        Console.Write("Isso");
+        Console.Write(" escreve na mesma linha");
+        }
     }
 }
+
+// Explicando o código :
+// Using System = Significa que podemos usar classes do Systesm namespace
+// namespace = usado para organizar o código e é um contêiner para classes e outros namespaces
+// As chaves {} marcam o início e o fim de um bloco de código
+// class = Container para dados e métodos que traz funcionalidade ao programa. Cada linha de código executada em C# deve estar dentro de uma classe
+// Main = Métod
+// Console = Classe do System namspace que possui um WriteLine() método que é utilizado para saída/impressão de texto
+// C# é Case Sensitive ou seja, diferencia Maúsculas de minúsculas
+/*
+    Comentário também
+*/
