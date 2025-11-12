17b2e422e136ca903ab54c9c116606bfc0c53998
Daniel <55755085+danzinho007@users.noreply.github.com>
2025-11-12 04:47:52 -0300

    mod

--- Aula01Ate10/Aula02/obj/Debug/net7.0/Aula02.AssemblyInfo.cs
+++ Aula01Ate10/Aula02/obj/Debug/net7.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa78721e3a92e524052022310d61fc503231fd94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71151626b14bf14aedeeb25a6c745643f5ab723")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
